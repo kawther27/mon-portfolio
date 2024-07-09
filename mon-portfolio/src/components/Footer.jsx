7dc7067a57837f3d71a8acd99a65b2242a1a078e
kawther27 <kawtherkhlif20@gmail.com>
2024-07-09 10:44:47 -0400

    Add Header and Footer components

---
+++ mon-portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Mon Portfolio</p>
+      <a href="https://github.com/kawther27">GitHub</a>
+      <a href="https://https://www.linkedin.com/in/kawther-khlif-8b5537280/">LinkedIn</a>
+    </footer>
+  )
+}
+
+export default Footer
